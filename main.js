6c487c7900a01ad99f2d5fe1c16e1ab6edb96b1b
Gabriela Peixoto <peixoto.sgabriela@gmail.com>
2025-02-18 19:31:52 -0300

    insere os arquivos em subpastas

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function onFormSubmit(e) {
   var certidao = null;
   var balanco = null;
   var dre = null;
-  var cartificacao = null;
+  var certificacao = null;
 
   for (var i = 0; i < itemResponses.length; i++) {
     var itemResponse = itemResponses[i];
@@ -17,16 +17,20 @@ function onFormSubmit(e) {
     // Logger.log("Resposta: " + response); // Log da resposta
 
     if (question == "Razão Social:") {
+      razaoSocialQuestion = JSON.stringify(question);
       razaoSocial = JSON.stringify(response);
     } else if (question == "Certidão Negativa de Débitos (CND)") {
+      certidaoQuestion = JSON.stringify(question);
       certidao = JSON.stringify(response);
     } else if (question == "Balanço Patrimonial") {
       balancoQuestion = JSON.stringify(question);
       balanco = JSON.stringify(response);
     } else if (question == "DRE") {
+      dreQuestion = JSON.stringify(question);
       dre = JSON.stringify(response);
     } else if (question == "Se a resposta anterior for sim, anexar Certificações:") {
-      cartificacao = JSON.stringify(response);
+      certificacaoQuestion = JSON.stringify(question);
+      certificacao = JSON.stringify(response);
     }
   }
 
@@ -34,51 +38,60 @@ function onFormSubmit(e) {
   // Logger.log("Certidão CND URL: " + certidao);
   // Logger.log("Balanço Patrimonial: " + balanco);
   // Logger.log("DRE URL: " + dre);
-  // Logger.log("DRE URL: " + cartificacao);
+  // Logger.log("DRE URL: " + certificacao);
 
-  var pastaRaiz = DriveApp.getFolderById("ID_PASTA_RAIZ"); // ID da sua pasta raiz
+  var pastaRaiz = DriveApp.getFolderById("1jA6j3r1tP983ihkaKtTHRMwGQVsqinJl"); // ID da sua pasta raiz
 
   // Crie a pasta com a Razão Social, se não existir
   var pastas = pastaRaiz.getFoldersByName(razaoSocial);
-  if (pastas.hasNext()) {
-    pastaEmpresa = pastas.next();
-  } else {
-    pastaEmpresa = pastaRaiz.createFolder(razaoSocial.replace(/"/g, ''));
-  }
+  var pastaEmpresa = pastas.hasNext() ? pastas.next() : pastaRaiz.createFolder(razaoSocial.replace(/"/g, ''));
+
 
   if (certidao != null) {
     var certidoes = certidao.split(",");
+    var pastasCampo = pastaEmpresa.getFoldersByName(certidaoQuestion);
+    var subpastaEmpresa = pastasCampo.hasNext() ? pastasCampo.next() : pastaEmpresa.createFolder(certidaoQuestion.replace(/"/g, ''));
+
     for (var i = 0; i < certidoes.length; i++) {
       var idArquivo = certidoes[i].replace(/[\[\]"]/g, '');
       Logger.log("Balanço Patrimonial: " + idArquivo);
-      salvarArquivo(idArquivo, pastaEmpresa);
+      salvarArquivo(idArquivo, subpastaEmpresa);
     }
   }
 
   if (balanco != null) {
     var balancos = balanco.split(",");
+    var pastasCampo = pastaEmpresa.getFoldersByName(balancoQuestion);
+    var subpastaEmpresa = pastasCampo.hasNext() ? pastasCampo.next() : pastaEmpresa.createFolder(balancoQuestion.replace(/"/g, ''));
+
     for (var i = 0; i < balancos.length; i++) {
       var idArquivo = balancos[i].replace(/[\[\]"]/g, '');
       Logger.log("Balanço Patrimonial: " + idArquivo);
-      salvarArquivo(idArquivo, pastaEmpresa);
+      salvarArquivo(idArquivo, subpastaEmpresa);
     }
   }
 
   if (dre != null) {
     var dres = dre.split(",");
+    var pastasCampo = pastaEmpresa.getFoldersByName(dreQuestion);
+    var subpastaEmpresa = pastasCampo.hasNext() ? pastasCampo.next() : pastaEmpresa.createFolder(dreQuestion.replace(/"/g, ''));
+
     for (var i = 0; i < dres.length; i++) {
       var idArquivo = dres[i].replace(/[\[\]"]/g, '');
       Logger.log("Balanço Patrimonial: " + idArquivo);
-      salvarArquivo(idArquivo, pastaEmpresa);
+      salvarArquivo(idArquivo, subpastaEmpresa);
     }
   }
 
-  if (cartificacao != null) {
-    var certificacoes = cartificacao.split(",");
+  if (certificacao != null) {
+    var certificacoes = certificacao.split(",");
+    var pastasCampo = pastaEmpresa.getFoldersByName(certificacaoQuestion);
+    var subpastaEmpresa = pastasCampo.hasNext() ? pastasCampo.next() : pastaEmpresa.createFolder(certificacaoQuestion.replace(/"/g, ''));
+
     for (var i = 0; i < certificacoes.length; i++) {
       var idArquivo = certificacoes[i].replace(/[\[\]"]/g, '');
       Logger.log("Balanço Patrimonial: " + idArquivo);
-      salvarArquivo(idArquivo, pastaEmpresa);
+      salvarArquivo(idArquivo, subpastaEmpresa);
     }
   }
 }
@@ -99,13 +112,8 @@ function salvarArquivo(idArquivo, pastaDestino) {
       // 4. Define o nome do Blob
       blob.setName(nomeArquivo);
 
-      // 5. Cria o arquivo na pasta de destino e verifica se arquivo já existe
-      var arquivosExistentes = pastaDestino.getFilesByName(nomeArquivo);
-      if (!arquivosExistentes.hasNext()) {
-        pastaDestino.createFile(blob);
-      } else {
-        Logger.log("Arquivo " + nomeArquivo + " já existe na pasta.");
-      }
+      // 5. Cria o arquivo na pasta de destino
+      pastaDestino.createFile(blob);
 
     } catch (error) {
       Logger.log("Erro ao salvar arquivo " + nomeArquivo + ": " + error);
